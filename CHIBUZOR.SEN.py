0cd9086ce4c7938f68d3d36d438d95a40eab78e3
zubi63888 <149195965+zubi63888@users.noreply.github.com>
2024-11-05 17:31:17 +0100

    Add files via upload

---
+++ CHIBUZOR.SEN.py
@@ -0,0 +1,9 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Print("HELLO WORLD")
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    Print("HELLO WORLD")
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> print("HELLO WORLD")
+HELLO WORLD
